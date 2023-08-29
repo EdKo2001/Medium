26d8bb6614c2e3f60da17bdcfcc8eaf1cbc74f49
Eduard Koshkelyan <eduardko2001@gmail.com>
2023-08-29 12:46:20 +0400

    refactor(react-compounds-components): enhance logic encapsulation and customization in Accordion using compound components

--- react-compound-components/src/components/Accordion/index.tsx
+++ react-compound-components/src/components/Accordion/index.tsx
@@ -1,31 +1,101 @@
-import React, { ReactNode, useState, createContext } from "react";
+import React, { FC, ReactNode, useState } from "react";
 
 import "./Accordion.css";
 
-// Create a context to share state between Accordion and AccordionItem
-interface AccordionContextType {
-  activeIndex: number;
-  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
-}
-
-export const AccordionContext = createContext<AccordionContextType | undefined>(
-  undefined
-);
-
+/**
+ * Props for the Accordion component.
+ */
 interface AccordionProps {
   children: ReactNode;
 }
 
-const Accordion: React.FC<AccordionProps> = ({ children }) => {
+/**
+ * Props for the AccordionItem component.
+ */
+interface AccordionItemProps {
+  index: number;
+  label: string;
+  children: ReactNode;
+  activeIndex?: number;
+  handleItemClick?: (index: number) => void;
+}
+
+/**
+ * A compound component representing an accordion.
+ */
+interface AccordionComponent extends FC<AccordionProps> {
+  Item: FC<AccordionItemProps>;
+}
+
+/**
+ * The Accordion component.
+ *
+ * @param {AccordionProps} props - The component props.
+ * @returns {JSX.Element} The rendered Accordion component.
+ */
+const Accordion: AccordionComponent = ({
+  children,
+}: AccordionProps): JSX.Element => {
   const [activeIndex, setActiveIndex] = useState<number>(-1);
+
+  /**
+   * Handle the click event on an accordion item.
+   *
+   * @param {number} index - The index of the clicked item.
+   */
+  const handleItemClick = (index: number) => {
+    setActiveIndex((prevActiveIndex) =>
+      prevActiveIndex === index ? -1 : index
+    );
+  };
 
   return (
     <div>
-      <AccordionContext.Provider value={{ activeIndex, setActiveIndex }}>
-        {children}
-      </AccordionContext.Provider>
+      {React.Children.map(children, (child, index) => {
+        if (React.isValidElement<AccordionItemProps>(child)) {
+          return React.cloneElement(child, {
+            index,
+            activeIndex,
+            handleItemClick,
+          });
+        }
+        return child;
+      })}
     </div>
   );
 };
+
+/**
+ * The AccordionItem component.
+ *
+ * @param {AccordionItemProps} props - The component props.
+ * @returns {JSX.Element} The rendered AccordionItem component.
+ */
+const AccordionItem: FC<AccordionItemProps> = ({
+  index,
+  label,
+  activeIndex,
+  handleItemClick,
+  children,
+}: AccordionItemProps): JSX.Element => {
+  /**
+   * Handle the click event on an accordion item button.
+   */
+  const handleClick = () => {
+    handleItemClick?.(index);
+  };
+
+  return (
+    <div className="accordion">
+      <button onClick={handleClick} className="accordion-button">
+        {label}
+      </button>
+      {activeIndex === index && <div>{children}</div>}
+    </div>
+  );
+};
+
+// Attach the AccordionItem component as a property of the Accordion component.
+Accordion.Item = AccordionItem;
 
 export default Accordion;
